c9a93c94d21acebb144ace837ff9adbcd5cf1b0c
codeunlu <you@example.com>
2021-07-16 23:30:12 +0300

    THY Eklendi

--- src/com/codeunlu/service/concretes/THYRezervasyon.java
+++ src/com/codeunlu/service/concretes/THYRezervasyon.java
@@ -6,7 +6,25 @@ import com.codeunlu.service.abstracts.BaseRezervasyonService;
 import java.util.Scanner;
 
 public class THYRezervasyon implements BaseRezervasyonService {
+    private int koltukNumarasi;
+
     @Override
     public void rezervasyonAl(Ucak ucak) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Koltik Tipi Seçiniz?");
+        System.out.println("1- Business \n2- Ekonomi");
+        int secim = scanner.nextInt();
+
+        System.out.println(ucak.getKoltukTuru());
+        while (true){
+            koltukNumarasi = scanner.nextInt();
+            boolean koltukDoluluk = ucak.getKoltuklar().get(koltukNumarasi);
+            if(!koltukDoluluk){
+                ucak.getKoltuklar().put(koltukNumarasi,true);
+                System.out.println(ucak.getKoltuklar() + "\n" + ucak.getKoltukTuru());
+            }else{
+                System.out.println("Koltuk malesef dolu!");
+            }
+        }
     }
 }
